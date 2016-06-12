c62b28015a262f8641d6b571c4fe8b5958e7f46f
Miguel Gonzalez <migonzalvar@gmail.com>
2016-06-12 19:11:03 +0200

    Move metaclass to base module

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,29 +1,6 @@
 from pysagec import models
 
 
-def test_field():
-    f = models.Field('tag')
-    assert f.__get__(None, None) is f
-    assert 'Field' in repr(f)
-
-
-def test_model_as_dict():
-    class MyModel(models.Model):
-        root_tag = 'root'
-        prop1 = models.Field('tag1')
-        prop2 = models.Field('tag2')
-
-    model = MyModel(prop1=42)
-    model.prop2 = 'foo'
-
-    assert model.prop1 == 42
-    assert {'root': [{'tag1': 42}, {'tag2': 'foo'}]} == model.as_dict()
-
-
-def test_model_default():
-    class MyModel(models.Model):
-        root_tag = 'root'
-        prop = models.Field('tag')
-
-    model = MyModel()
-    assert model.prop is None
+def test_models_are_instantiable():
+    a = models.AuthInfo()
+    assert isinstance(a, models.Model)
