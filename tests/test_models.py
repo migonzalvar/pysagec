29221a6e8d47a15ce917eae249420b1a9c068a36
Miguel Gonzalez <migonzalvar@gmail.com>
2016-06-12 17:00:09 +0200

    Use builtin dict comparison

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,15 +4,11 @@ from pysagec import models
 def test_auth_info():
     auth_info = models.AuthInfo()
     data = auth_info.as_dict()
-    assert 'mrw:AuthInfo' in data
-    elements = data['mrw:AuthInfo']
-    assert len(elements) == 5
-    keys = [
-      'mrw:CodigoFranquicia',
-      'mrw:CodigoAbonado',
-      'mrw:CodigoDepartamento',
-      'mrw:UserName',
-      'mrw:Password',
-    ]
-    for key, element in zip(keys, elements):
-        assert key in element
+    expected = {'mrw:AuthInfo': [
+        {'mrw:CodigoFranquicia': ''},
+        {'mrw:CodigoAbonado': ''},
+        {'mrw:CodigoDepartamento': ''},
+        {'mrw:UserName': ''},
+        {'mrw:Password': ''},
+    ]}
+    assert expected == data
