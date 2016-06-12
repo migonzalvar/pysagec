7e67996a32eb5914eaedd6fb6d38a8789c35eb3b
Miguel Gonzalez <migonzalvar@gmail.com>
2016-06-12 17:43:28 +0200

    Add properties on AuthInfo

--- pysagec/models.py
+++ pysagec/models.py
@@ -1,9 +1,17 @@
 class AuthInfo:
+    def __init__(self, franchise_code, subscriber_code, departament_code,
+                 username, password):
+        self.subscriber_code = subscriber_code
+        self.franchise_code = franchise_code
+        self.departament_code = departament_code
+        self.username = username
+        self.password = password
+
     def as_dict(self):
         return {'mrw:AuthInfo': [
-            {'mrw:CodigoFranquicia': ''},
-            {'mrw:CodigoAbonado': ''},
-            {'mrw:CodigoDepartamento': ''},
-            {'mrw:UserName': ''},
-            {'mrw:Password': ''},
+            {'mrw:CodigoFranquicia': self.franchise_code},
+            {'mrw:CodigoAbonado': self.subscriber_code},
+            {'mrw:CodigoDepartamento': self.departament_code},
+            {'mrw:UserName': self.username},
+            {'mrw:Password': self.password},
         ]}
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,13 +2,18 @@ from pysagec import models
 
 
 def test_auth_info():
-    auth_info = models.AuthInfo()
+    kwargs = {'franchise_code': '123456',
+              'subscriber_code': 'subscriber_code',
+              'departament_code': 'departament_code',
+              'username': 'username',
+              'password': 'password'}
+    auth_info = models.AuthInfo(**kwargs)
     data = auth_info.as_dict()
     expected = {'mrw:AuthInfo': [
-        {'mrw:CodigoFranquicia': ''},
-        {'mrw:CodigoAbonado': ''},
-        {'mrw:CodigoDepartamento': ''},
-        {'mrw:UserName': ''},
-        {'mrw:Password': ''},
+        {'mrw:CodigoFranquicia': kwargs['franchise_code']},
+        {'mrw:CodigoAbonado': kwargs['subscriber_code']},
+        {'mrw:CodigoDepartamento': kwargs['departament_code']},
+        {'mrw:UserName': kwargs['username']},
+        {'mrw:Password': kwargs['password']},
     ]}
     assert expected == data
