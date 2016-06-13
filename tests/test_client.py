604597d710441f26c03228869938539756788d79
Miguel Gonzalez <migonzalvar@gmail.com>
2016-06-13 22:48:44 +0200

    Pass TEST_URL using environment

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,8 +1,15 @@
 import os
+from urllib.parse import urlsplit, parse_qs
+
 import pytest
 
 from pysagec import Client
 from pysagec.models import AuthInfo, PickupInfo, ServiceInfo
+
+
+def key_or_none(qs, key):
+    iterable = qs.get(key, [None])
+    return iterable[0]
 
 
 class MockModel:
@@ -12,7 +19,23 @@ class MockModel:
 
 @pytest.fixture
 def test_client():
-    return Client('sagec-test.mrw.es', AuthInfo())
+    url = os.environ.get(
+        'TEST_URL',
+        '//user:pass@example.com/?franchise=12&subscriber=34&department=56'
+    )
+
+    url = urlsplit(url)
+    qs = parse_qs(url.query)
+
+    hostname = url.hostname
+    kwargs = {
+        'username': url.username,
+        'franchise_code': key_or_none(qs, 'franchise'),
+        'subscriber_code': key_or_none(qs, 'subscriber'),
+        'departament_code': key_or_none(qs, 'department'),
+    }
+    auth_info = AuthInfo(**kwargs)
+    return Client(hostname, auth_info)
 
 
 def test_client_init():
@@ -27,8 +50,8 @@ def test_client_make_http_request():
 
 
 @pytest.mark.skipif(
-    os.environ.get('TEST_PRE') != 'true',
-    reason='TEST_PRE=true is no set',
+    not os.environ.get('TEST_URL', False),
+    reason='TEST_URL environment is no set',
 )
 def test_send(test_client):
     pickup_info = PickupInfo()
