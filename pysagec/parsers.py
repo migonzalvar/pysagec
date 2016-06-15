b0a386a7031feabbf0ae10fe57de5eabcdd929b2
Miguel Gonzalez <migonzalvar@gmail.com>
2016-06-15 20:50:48 +0200

    Add XMLParser

---
+++ pysagec/parsers.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+import xml.etree.ElementTree as etree
+
+
+class ParseError(Exception):
+    pass
+
+
+class XMLParser:
+    def parse(self, stream):
+        parser = etree.XMLParser(encoding='utf-8')
+
+        try:
+            tree = etree.parse(BytesIO(stream), parser)
+        except etree.ParseError as exc:
+            raise ParseError(exc.msg)
+
+        root = tree.getroot()
+        data = self._xml_convert(root, unwrap=True)
+        return {root.tag: data}
+
+    def _xml_convert(self, element, unwrap=False):
+        children = list(element)
+
+        if len(children) > 0:
+            data = []
+            for child in children:
+                data.append({child.tag: self._xml_convert(child)})
+            return {element.tag: data} if not unwrap else data
+
+        # len(children) == 0
+        return element.text
---
+++ tests/test_parsers.py
@@ -0,0 +1,69 @@
+import pytest
+
+from pysagec import parsers
+
+
+def test_parser_simple():
+    stream = (
+        b'<?xml version="1.0" encoding="utf-8"?>\n'
+        b'<root></root>'
+    )
+    parser = parsers.XMLParser()
+    data = parser.parse(stream)
+    assert {'root': None} == data
+
+
+def test_parser_simple_with_namespace():
+    stream = (
+        b'<?xml version="1.0" encoding="utf-8"?>\n'
+        b'<root xmlns="http://example.com"></root>'
+    )
+    parser = parsers.XMLParser()
+    data = parser.parse(stream)
+    assert {'{http://example.com}root': None} == data
+
+
+def test_parser_one_children():
+    stream = (
+        b'<?xml version="1.0" encoding="utf-8"?>\n'
+        b'<root><tag>Value</tag></root>'
+    )
+    parser = parsers.XMLParser()
+    data = parser.parse(stream)
+    assert {'root': [{'tag': 'Value'}]} == data
+
+
+def test_parser_one_children_with_two_namespaces():
+    stream = (
+        b'<?xml version="1.0" encoding="utf-8"?>\n'
+        b'<root'
+        b' xmlns="http://example.com"'
+        b' xmlns:ns="http://example.org">'
+        b'<ns:tag>Value</ns:tag>'
+        b'</root>'
+    )
+    parser = parsers.XMLParser()
+    data = parser.parse(stream)
+    assert ({
+        '{http://example.com}root': [{'{http://example.org}tag': 'Value'}]
+    }) == data
+
+
+def test_parser_many_children():
+    stream = (
+        b'<?xml version="1.0" encoding="utf-8"?>\n'
+        b'<root><tag1>Value 1</tag1><tag2>Value 2</tag2></root>'
+    )
+    parser = parsers.XMLParser()
+    data = parser.parse(stream)
+    assert {'root': [{'tag1': 'Value 1'}, {'tag2': 'Value 2'}]} == data
+
+
+def test_parser_error():
+    bad_stream = (
+        b'<?xml version="1.0" encoding="utf-8"?>\n'
+        b'<root><tag1>Value 1</tag2>'
+    )
+    parser = parsers.XMLParser()
+    with pytest.raises(parsers.ParseError):
+        parser.parse(bad_stream)
