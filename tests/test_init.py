36a85c6d1888c9ec4466f3607e1767c97fbf48b1
Miguel Gonzalez <migonzalvar@gmail.com>
2016-06-24 20:18:51 +0200

    Remove import all

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,10 +1,10 @@
-from pysagec import *  # NOQA: on purpose to detect __all__ regressions
+import pysagec
 
 
 def test_create_client():
     url = '//user:pass@example.com/?franchise=12&subscriber=34&department=56'
-    client = create_client(url)  # noqa: F405
-    assert isinstance(client, Client)  # noqa: F405
+    client = pysagec.create_client(url)
+    assert isinstance(client, pysagec.Client)
     assert client.auth_info.username == 'user'
     assert client.auth_info.password == 'pass'
     assert client.auth_info.franchise_code == '12'
