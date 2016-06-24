e90a8beb08dad83ce3d3e43b8f2329211b903d1f
Miguel Gonzalez <migonzalvar@gmail.com>
2016-06-24 12:42:20 +0200

    Ignore F405 may be indefined explicitly

--- tests/test_init.py
+++ tests/test_init.py
@@ -3,8 +3,8 @@ from pysagec import *  # NOQA: on purpose to detect __all__ regressions
 
 def test_create_client():
     url = '//user:pass@example.com/?franchise=12&subscriber=34&department=56'
-    client = create_client(url)
-    assert isinstance(client, Client)
+    client = create_client(url)  # noqa: F405
+    assert isinstance(client, Client)  # noqa: F405
     assert client.auth_info.username == 'user'
     assert client.auth_info.password == 'pass'
     assert client.auth_info.franchise_code == '12'
