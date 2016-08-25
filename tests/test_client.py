d6174b194f56eb659c8b77ce78551ab02af318b8
Miguel Gonzalez <migonzalvar@gmail.com>
2016-08-25 18:31:44 +0200

    Refactor using pytest parametrized tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,8 @@
 from contextlib import contextmanager
 from unittest.mock import patch, MagicMock
 
+import pytest
+
 from pysagec import Client
 
 
@@ -38,70 +40,74 @@ def test_client_make_http_request():
     assert req.get_method() == 'POST'
 
 
-def test_send_with_mock():
-    body_response = (
-        b'<?xml version="1.0" encoding="utf-8"?>'
-        b'<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"'
-        b' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
-        b' xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
-        b'<soap:Body>'
-        b'<TransmEnvioResponse xmlns="http://www.mrw.es/">'
-        b'<TransmEnvioResult>'
-        b'<Estado>1</Estado>'
-        b'<Mensaje>1) La fecha de recogida solicitada es 01/08/2016.</Mensaje>'
-        b'<NumeroSolicitud>0330550767020160615223238261</NumeroSolicitud>'
-        b'<NumeroEnvio>033050000050</NumeroEnvio>'
-        b'<Url>http://sagec-test.mrw.es/Panel.aspx</Url>'
-        b'</TransmEnvioResult>'
-        b'</TransmEnvioResponse>'
-        b'</soap:Body>'
-        b'</soap:Envelope>'
-    )
+@pytest.mark.parametrize('body_response,status,shipping_number', [
+    (
+        (b'<?xml version="1.0" encoding="utf-8"?>'
+         b'<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"'
+         b' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
+         b' xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
+         b'<soap:Body>'
+         b'<TransmEnvioResponse xmlns="http://www.mrw.es/">'
+         b'<TransmEnvioResult>'
+         b'<Estado>1</Estado>'
+         b'<Mensaje>La fecha de recogida solicitada es 01/08/2016.</Mensaje>'
+         b'<NumeroSolicitud>0330550767020160615223238261</NumeroSolicitud>'
+         b'<NumeroEnvio>033050000050</NumeroEnvio>'
+         b'<Url>http://sagec-test.mrw.es/Panel.aspx</Url>'
+         b'</TransmEnvioResult>'
+         b'</TransmEnvioResponse>'
+         b'</soap:Body>'
+         b'</soap:Envelope>'),
+        '1',
+        '033050000050',
+    ),
+])
+def test_send(body_response, status, shipping_number):
     with patched_client(body_response) as client:
         response = client.send(MockModel(), MockModel())
-    assert '1' == response.status
-    assert '033050000050' == response.shipping_number
+    assert status == response.status
+    assert shipping_number == response.shipping_number
 
 
-def test_get_label_with_mock():
-    body_response = (
-        b'<?xml version="1.0" encoding="utf-8"?>'
-        b'<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"'
-        b' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
-        b' xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
-        b'<soap:Body>'
-        b'<GetEtiquetaEnvioResponse xmlns="http://www.mrw.es/">'
-        b'<GetEtiquetaEnvioResult>'
-        b'<Estado>1</Estado>'
-        b'<Mensaje/>'
-        b'<EtiquetaFile>AA==</EtiquetaFile>'
-        b'</GetEtiquetaEnvioResult>'
-        b'</GetEtiquetaEnvioResponse>'
-        b'</soap:Body>'
-        b'</soap:Envelope>'
-    )
+@pytest.mark.parametrize('body_response,status,file', [
+    (
+        (b'<?xml version="1.0" encoding="utf-8"?>'
+         b'<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"'
+         b' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
+         b' xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
+         b'<soap:Body>'
+         b'<GetEtiquetaEnvioResponse xmlns="http://www.mrw.es/">'
+         b'<GetEtiquetaEnvioResult>'
+         b'<Estado>1</Estado>'
+         b'<Mensaje/>'
+         b'<EtiquetaFile>AA==</EtiquetaFile>'
+         b'</GetEtiquetaEnvioResult>'
+         b'</GetEtiquetaEnvioResponse>'
+         b'</soap:Body>'
+         b'</soap:Envelope>'),
+        '1',
+        'AA==',
+    ),
+    (
+        (b'<?xml version="1.0" encoding="utf-8"?>'
+         b'<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"'
+         b' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
+         b' xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
+         b'<soap:Body>'
+         b'<GetEtiquetaEnvioResponse xmlns="http://www.mrw.es/">'
+         b'<GetEtiquetaEnvioResult>'
+         b'<Estado>0</Estado>'
+         b'<Mensaje/>'
+         b'</GetEtiquetaEnvioResult>'
+         b'</GetEtiquetaEnvioResponse>'
+         b'</soap:Body>'
+         b'</soap:Envelope>'),
+        '0',
+        None,
+    ),
+])
+def test_get_label(body_response, status, file):
     with patched_client(body_response) as client:
         response = client.get_label(MockLabelRequest())
-    assert '1' == response.status
-    assert 'AA==' == response.file
-
-
-def test_get_label_with_error_with_mock():
-    body_response = (
-        b'<?xml version="1.0" encoding="utf-8"?>'
-        b'<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"'
-        b' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'
-        b' xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
-        b'<soap:Body>'
-        b'<GetEtiquetaEnvioResponse xmlns="http://www.mrw.es/">'
-        b'<GetEtiquetaEnvioResult>'
-        b'<Estado>0</Estado>'
-        b'<Mensaje/>'
-        b'</GetEtiquetaEnvioResult>'
-        b'</GetEtiquetaEnvioResponse>'
-        b'</soap:Body>'
-        b'</soap:Envelope>'
-    )
-    with patched_client(body_response) as client:
-        response = client.get_label(MockLabelRequest())
-    assert '0' == response.status
+    assert status == response.status
+    assert file == response.file
