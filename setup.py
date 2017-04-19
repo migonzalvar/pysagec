84440f73985c7c497d48f83dc795229e0e3fac89
Miguel Gonzalez <migonzalvar@gmail.com>
2017-04-19 18:56:29 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='pysagec',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/migonzalvar/pysagec',
     license='MIT',
     author='Miguel Gonzalez',
